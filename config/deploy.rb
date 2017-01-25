17915c896bb6eb65519e296d690499560679b821
Takashi Kokubun <takashi-kokubun@cookpad.com>
2017-01-26 00:29:42 +0900

    Allow specifying branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'railsapp'
 set :repo_url, 'git@github.com:k0kubun/railsapp.git'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, ENV.fetch('BRANCH', `git rev-parse --abbrev-ref HEAD`.chomp)
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deploy/railsapp-cap'

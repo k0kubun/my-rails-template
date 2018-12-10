f6d6066fe025cd76a3e3b9072d2182322a2edf23
Takashi Kokubun <takashikkbn@gmail.com>
2018-12-10 20:11:43 +0900

    Add 5 pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'application#index'
+  get '/page1' => 'application#page1'
+  get '/page2' => 'application#page2'
+  get '/page3' => 'application#page3'
+  get '/page4' => 'application#page4'
+  get '/page5' => 'application#page5'
 end
